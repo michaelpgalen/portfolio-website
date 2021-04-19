bbd16c0b1aeb417a0eb7b24c75ce8b6320f2e11d
michaelpgalen <52191377+michaelpgalen@users.noreply.github.com>
2021-04-19 16:16:49 -0700

    add jquery and js interactions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Welcome to my site!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+    }else {
+      x.className = 'navtoggle';
+    }
+  }
