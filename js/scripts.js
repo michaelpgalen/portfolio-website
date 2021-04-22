821bc89658bc515d2ef700989ed5910c3a3baefa
michaelpgalen <52191377+michaelpgalen@users.noreply.github.com>
2021-04-21 17:34:27 -0700

    fixes menu nav toggle bug. Adds css sections

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ console.log("Welcome to my site!");
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
     }else {
       x.className = 'navtoggle';
     }
