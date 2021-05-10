56b088cc26cb705c8050e37376055cabc2aa7623
michaelpgalen <52191377+michaelpgalen@users.noreply.github.com>
2021-05-10 16:26:09 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
-console.log("Welcome to my site!");
-
-function menuToggle() {
+function menuToggle () {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
-    }else {
-      x.className = 'navtoggle';
-    }
+  } else {
+    x.className = 'navtoggle';
   }
+}
+// Get the button:
+// var topbutton = document.getElementById('topBtn');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() }
+
+function scrollFunction () {
+  var topbutton = document.getElementById('topBtn');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = 'block';
+  } else {
+    topbutton.style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
